0d0c531fd7e073c841356b0c39a0c582ea55af66
pmonika20 <128023109+pmonika20@users.noreply.github.com>
2023-03-19 10:52:59 +1100

    test

--- src/main/java/com/example/demo/Newpe1Application.java
+++ src/main/java/com/example/demo/Newpe1Application.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,7 @@ public class Newpe1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Newpe1Application.class, args);
-		System.out.println("completed");
+		//System.out.println("completed");
 	}
 
 	@RequestMapping("/")
