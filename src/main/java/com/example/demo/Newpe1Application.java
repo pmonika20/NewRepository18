bbf5b76ae15bf5eb04ded3f754221a4041217df9
pmonika20 <pandeymonikajugujugu@gmail.com>
2023-03-18 21:52:55 +1100

    commit

---
+++ src/main/java/com/example/demo/Newpe1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Newpe1Application {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Gullaa dekho website ban agayi";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Newpe1Application.class, args);
+		System.out.println("completed");
+	}
+
+}
--- src/main/java/com/example/demo/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.bean.Subject;
-
-public interface SubjectRepository extends CrudRepository<Subject, Integer> {
-
-}
