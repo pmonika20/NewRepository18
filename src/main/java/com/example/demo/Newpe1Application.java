b0486993a687d8764c9159801867c8cb70a84b95
pmonika20 <128023109+pmonika20@users.noreply.github.com>
2023-03-19 11:25:21 +1100

    Okay

--- src/main/java/com/example/demo/Newpe1Application.java
+++ src/main/java/com/example/demo/Newpe1Application.java
@@ -2,22 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Newpe1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Newpe1Application.class, args);
-		//System.out.println("completed");
-	}
-
-	@RequestMapping("/")
+		@GetMapping("/Message")
 	public String message() {
 		return "Gullaa dekho website ban agayi";
 	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newpe1Application.class, args);
+		System.out.println("completed");
+	}
+
+
 
 
 }
